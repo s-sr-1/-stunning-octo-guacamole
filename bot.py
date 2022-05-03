01a76000c48f3a2d1a2dd10c2da7d3b5acbba5e6
s-sr-1 <104184605+s-sr-1@users.noreply.github.com>
2022-05-03 14:54:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from bot import LOG, app, chats_data, from_chats
 
 LOG.info("Welcome, this is the telegram-message-forwarder-bot. main routine...")
 
-@app.on_message(filters.chat(from_chats) & filters.photo)
+@app.on_message(filters.chat(from_chats))
 def work(client, message):
     try:
         for chat in chats_data[message.chat.id]:
